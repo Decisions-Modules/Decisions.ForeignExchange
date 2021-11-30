30931f633ee0e92ee80d961ca881ce7910ef8cfe
jeff-decisions <72927224+jeff-decisions@users.noreply.github.com>
2021-11-30 15:18:59 -0500

    Correcting Upgrade code

--- Decisions.ForEx/ForeignExchangeInitializer.cs
+++ Decisions.ForEx/ForeignExchangeInitializer.cs
@@ -17,27 +17,19 @@ namespace Decisions.ForEx
             const string oldSettingsClassName = "ForiegnExchangeSettings";
             const string oldSettingsTableName = "foriegn_exchange_settings";
             const string newSettingsTableName = "foreign_exchange_settings";
-
+    
             if (DynamicORM.DatabaseDriver.DoesTableExist(DynamicORM.ConnectionString, oldSettingsTableName))
             {
-                ORM<ForiegnExchangeSettings> orm = new ORM<ForiegnExchangeSettings>();
-
-                var oldSettings = orm.Fetch().FirstOrDefault();
-                if (oldSettings != null)
-                {
-                    ForeignExchangeSettings fes = ModuleSettingsAccessor<ForeignExchangeSettings>.GetSettings();
-                    fes.UserId = oldSettings.userId;
-                    fes.Password = oldSettings.password;
-                    fes.ApiKey = oldSettings.apiKey;
-                    fes.UseHttps = oldSettings.useHttps;
-                    ModuleSettingsAccessor<ForeignExchangeSettings>.SaveSettings();
-                    
-                    string sqlQuery = $@"ALTER TABLE entity_header_data DISABLE TRIGGER entity_header_data_delete_trigger;
-                                        DELETE FROM entity_header_data WHERE entity_type_short_name='{oldSettingsClassName}';
-                                        DROP TABLE {oldSettingsTableName};
-                                        ALTER TABLE entity_header_data ENABLE TRIGGER entity_header_data_delete_trigger;";
-                    orm.RunQuery(sqlQuery);
-                }
+                ORM<ForeignExchangeSettings> orm = new ORM<ForeignExchangeSettings>();
+                ModuleSettingsAccessor<ForeignExchangeSettings>.SaveSettings();
+                string sqlUpdate =
+                    $@"UPDATE {newSettingsTableName} SET user_id=ol.user_id, password=ol.password, api_key=ol.api_key, use_https=ol.use_https FROM {oldSettingsTableName} as ol WHERE {newSettingsTableName}.entity_name=ol.entity_name;";
+                orm.RunQuery(sqlUpdate);
+                string sqlRemove = $@"ALTER TABLE entity_header_data DISABLE TRIGGER entity_header_data_delete_trigger;
+                                    DELETE FROM entity_header_data WHERE entity_type_short_name='{oldSettingsClassName}';
+                                    DROP TABLE {oldSettingsTableName};
+                                    ALTER TABLE entity_header_data ENABLE TRIGGER entity_header_data_delete_trigger;";
+                orm.RunQuery(sqlRemove);
             }
         }
     }
